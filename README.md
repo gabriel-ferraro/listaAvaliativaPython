541cf38131dbed14b7191bc3d6139e642848f210
Gabriel Ferraro Severino <gabrielsev_2002@hotmail.com>
2023-09-09 20:17:52 -0300

    exercícios finalizados

---
+++ README.md
@@ -0,0 +1,34 @@
+# Exercícios avaliativos realizados na disicplina de tópicos especiais de software.
+
+Foi adotado desenvolvimento orientado a objetos para resolução dos exercícios (minha preferência). Alguns dos requisitos foram levemente adaptados mas todos coportamentos esperados estão presentes nos programas.
+
+Estratégia para resolução dos exercícios foram descritas nas classes, comentários com Docstrings feitos para classes e métodos.
+
+* Foram atentdidos todos requisitos da lista.
+* Para gerar os comentários, foi utilizado um plugin auxiliar no vsc chamado autoDocstring - Python Docstring Generator.
+* Imagens de exemplo dos programas sendo executados abaixo.
+* libs utilizadas:
+    random - para adquirir um arquivo randômico no ex3.
+    sys - para finalizar o programa no ex4.
+
+# Programas em execução:
+
+## ex1 - jogo da velha 4x4
+
+![print do jogo da velha 4x4](./images/ex1_tic_tac_toe_4x4.png)
+
+## ex2 - jogo da velha NxN
+
+![print do jogo da velha NxN](./images/ex2_tic_tac_toe_NxN.png)
+
+## ex3 - Termo
+
+[print do jogo Termo](./images/ex3_termo.png)
+
+## ex4 - simulador de banco de dados em memória
+
+* Cadastro de usuários
+[print do simulador de banco de dados em memória (cadastro)](./images/ex4_cadastro.png)
+
+* Exibição de usuários
+[print do simulador de banco de dados em memória (exibição)](./images/ex4_pesquisas.png)
--- ex4.py
+++ ex4.py
@@ -1,120 +1,138 @@
 import sys
 
-"""
-Ao iniciar, o programa deve solicitar ao usuário os nomes dos campos que serão obrigatórios para os cadastros.
-Na sequência, deve mostrar o menu e iniciar o fluxo normal de execução.
-
-Opção 1. Crie uma função cadastrar_usuario para cadastrar um usuário de maneira flexível.
-A função deve receber uma tupla com os campos obrigatórios para cadastro.
-Estes campos devem ser definidos globalmente em tempo de execução pelo usuário.
-(opção anterior) O usuário pode cadastrar quantos campos quiser além dos obrigatórios até digitar "sair".Deve então retornar ao menu.
-A função deve retornar o dicionário gerado e armazenar no dicionário global chamado banco_usuarios
-
-Opção 2. Criar uma outra função imprimir_usuarios com 4 possibilidades de invocação:- Caso a função não receba argumentos, deve imprimir todos os usuários 
-com todas suasinfos.- Caso receba vários nomes, deve imprimir todos dados de todos os usuários com os 
-nomesespecificados.exemplo: imprimir_usuarios("alberto", "joaquina", "enzo", "valentina")- Caso receba uma série de 
-elementos campo e valor deve imprimir apenas os dadoscompletosde todos os usuários que satisfazem as condições:
-- Receber nomes e campos, valor: apenas nos usuários listados, imprimir os dados dosusuários quesatisfazem às condições dadas. 
-(usar *args e **kwargs facilita a definção da fun=ção)exemplo de execução da opção 
-
-2:1 - impirmir todos2 - filtrar por nomes3 - filtrar por campos4 - filtrar por nomes e campos2alberto, maria, enzo>> 
-deve imprimir os dados de alberto, maria e enzooutro exemplo3digite o campo de busca:endereçodigite o endereçoxv de 
-novembromais algum campo:idadedigite a idade20mais algum campo?sair
-
->> Deve imprimir os dados dos usuários que possuem o endereço xv de novembro a e idade20. Retorna ao menu inicial outro exemplo
-:4digite os nomesalberto, maria, josedigite os camposidadedigte a idade10outro camposair>> 
-deve imprimir os dados dos usuários, entre alberto, maria e josé, que possuem 10 anos.Retorna ao menu inicial.
-"""
-
 class Fake_database():
     """_summary_
-
+        Simula um banco de dados salvando usuários em memória. Ao iniciar o programa o usuário deve inserir valores que servirão como chaves obrigatórias,
+        que devem ser sempre preenchidas ao registrar um novo usuário. Após a primeira etapa o programa mostra um menu de escolhas onde o usuário pode registrar novos usuários,
+        inserir campos opcionais e 'salvar' o usuário; A outra opção permite pesquisar por todos usuários salvos na lista de tuplas.
     """
     def __init__(self):
-
-        self.user_bank = ()
-        self.obligatory_fields_tuple = ()
-
-    def _validate_values_for_tuple(user_entry) -> tuple:
         """_summary_
-            Limpa input removendo espaços em branco e adquirindo chaves separadas por vírgula.
-        Args:
-            user_entry (str): Input com valores do usuário separados por vírgula.
-
-        Returns:
-            tuple: tupla das chaves informadas pelo usuário.
+            Inicialzia uma lista vazia para representar o 'banco' de usuários em memória, mantém as chaves obrigatórias inseridas no início do programa.
         """
-        return [field.strip() for field in user_entry.split(',')]
+        self.user_bank = [] # Armazena em memória os usuários criados.
+        self.obligatory_fields_tuple = () # Tupla de campos obrigatórios requisitdados no início do programa.
     
-    def _get_user_obligatory_fields_tuple(self) -> tuple:
+    def _get_user_obligatory_fields_tuple(self) -> None:
         """_summary_
             Adquire os campos obrigatórios por input. e usuário digitar nenhum retorna tupla vazia.
         Returns:
             obligatory_tuple: tupla com campos obrigatórios inseridos pelo usuário (validados) ou tupla vazia.
         """
-        obligatory_tuple = ()
+        tuple_for_keys = tuple()
         while True:
-            user_entry = input('Digite os nomes dos campos separados por vírgula (sem espaços em branco) ou "nenhum" para finalizar sem adicionar campos: ')
+            user_entry = input('Digite os nomes dos campos separados por vírgula (sem espaços em branco) ou "nenhum" para finalizar sem adicionar campos: ').lower()
             # Limpa input removendo espaços em branco e adquirindo chaves separadas por vírgula.
-            field_names = self._validate_values_for_tuple(user_entry)
+            field_names = [field.strip() for field in user_entry.split(',')]
             # Se usuário escreveu nenhum não cria chaves e retorna tupla vazia.
             if ('nenhum' in field_names):
                 break
             # Quebra loop se não houverem chaves vazias, retornando a tupla validada.
-            elif all(field_names): 
-                obligatory_tuple = tuple(field_names) 
+            elif(all(field_names)): 
+                tuple_for_keys = tuple(field_names) 
                 break
             else: 
                 print("Por favor, insira nomes válidos para todos os campos.")
-        return obligatory_tuple
+        print(f'Tupla de campos obrigatórios: {tuple_for_keys}')
+        self.obligatory_fields_tuple = tuple_for_keys
 
-    def _register_user(self, obligatory_fields_tuple) -> None:
-        non_obligatory_tuple = ()
-        if (input('Deseja inserir campos não obrigatórios para usuário? (s/n): ').upper == 'S'):
-            while True:
-                user_entry = input('Digite os nomes dos campos separados por vírgula (sem espaços em branco) ou "nenhum" não adicionar campos: ')
-                # Se usuário escreveu nenhum não cria chaves e retorna tupla vazia.
-                if ('nenhum' in field_names):
-                    break
-        return 
-
-    def _print_main_menu(self) -> None:
-        print('Fake DB:')
-        print('-- Opção 1 -- Regitrar novo usuário')
-        print('-- Opção 2 -- Imprimir usuários.')
-        print('-- Opção 0 -- Sair')
+    def _register_user(self) -> None:
+        """_summary_
+        """
+        user = {}
+        # Checa se tupla de itens obrigatórios tem itens.
+        if (self.obligatory_fields_tuple):
+            # Pede para preencher os campos obrigatórios.
+            for field in self.obligatory_fields_tuple:
+                user[field] = input(f"Digite o valor para o campo '{field}': ")
+        
+        # Pede para preencher os campos opcionais.
+        while True:
+            optional_field = input("Digite o nome de um campo opcional (ou 'sair' para encerrar): ")
+            # Quebra loop.
+            if optional_field.upper() == 'SAIR':
+                break
+            user[optional_field] = input(f"Digite o valor para o campo '{optional_field}': ")
+        # Adiciona o usuário ao banco de usuários.
+        self.user_bank.append(user)
+        print(f'Usuário cadastrado com sucesso: {user}')
     
-    def _print_search_user_menu(self):
-        print('pEsquisa de usuários cadastrados. Escolha:')
-        print('-- Opção 1 imprimir todos usuários -- ')
-        print('-- Opção 2  -- ')
-        print('-- Opção 3 -- ')
-        print('-- Opção 4 -- ')
-        print('-- Opção 5 -- Voltar para tela inicial')
-        print('-- Opção 6 -- Encerrar programa')
+    def _print_users(self, *args, **kwargs) -> None:
+        """_summary_
+            Identifica argmentos recebidos. Se não receber argumentos faz print de todos dados, se receber nomes retorna todos dados dos usuários recebidos,
+            se receber argumentos de chave e valor, faz print das informações dos usuários que combinam.
+        """
+        # Checa condições.
+        if (not args and not kwargs):
+            # Caso a função não receba argumentos, imprimir todos os usuários com todas as informações
+            for user in self.user_bank:
+                print(user)
+        else:
+            # Caso receba argumentos, filtrar os usuários com base nas condições fornecidas
+            filtered_users = []
+            for user in self.user_bank:
+                # Por default atende condições para fazer pesquisa.
+                meet_conditions = True
+                # Filtrar por nomes
+                if (args):
+                    if (user.get("nome") not in args):
+                        meet_conditions = False
+                # Filtrar por campos e valores
+                for field, value in kwargs.items():
+                    if (field not in user or user[field] != value):
+                        meet_conditions = False
+                if (meet_conditions):
+                    filtered_users.append(user)
+            # Imprimir os usuários filtrados
+            print('\nDados identificados: ')
+            for user in filtered_users:
+                print(user)
 
-    def _select_user_search(self):
+
+    def _select_user_search(self) -> None:
         """_summary_
             Método de controle do menu de busca.
         """
         option  = ''
         while True:
-            self._print_search_user_menu()
+            # Print do menu de pesquisa.
+            print('Menu de pesquisa de usuários cadastrados:')
+            print('-- Opção 1 -- Pesquisar todos usuários')
+            print('-- Opção 2 -- Insira os nomes de usuários para ver seus dados')
+            print('-- Opção 3 -- Insira chaves-valores para adquirir os dados dos usuários semelhantes')
+            print('-- Opção 4 -- Voltar para o menu principal.')
+            print('-- Opção 0 -- Encerrar programa')
+            option = input("Escolha uma opção: ")
             if(option == '1'):
-                
+                self._print_users()
             elif(option == '2'):
-
+                names = input('Insira o nome dos usuários, separado por vírgula: ')
+                names = [name.strip() for name in names.split(',') if name.strip()]
+                # Verifica se a lista de nomes não está vazia após a validação.
+                if (names):
+                    self._print_users(*names)
+                else:
+                    print("Nenhum nome válido inserido.")
             elif(option == '3'):
-
+                fields = input('Insira os campos de busca, separado por vírgula: ')
+                fields = [field.strip() for field in fields.split(',') if field.strip()]
+                kwargs = {}
+                # Verifica se a lista de campos não está vazia após a validação.
+                if (fields):
+                    for field in fields:
+                        value = input(f'Digite o valor para o campo "{field}": ')
+                        kwargs[field] = value
+                    self._print_users(**kwargs)
             elif(option == '4'):
-
-            elif(option == '5'):
                 # Volta para menu principal.
+                print('Voltando para o menu principal.')
                 break
-            elif(option == '6'):
+            elif(option == '0'):
                 # Encerra o programa
+                print("Finalizando.")
                 sys.exit()
-
+            else:
+                print('Escolha não reconhecida.')
 
     def start(self) -> None:
         """_summary_
@@ -124,16 +142,19 @@ class Fake_database():
         self._get_user_obligatory_fields_tuple()
         option = ''
         while True:
-            # Faz print do menu inicial.
-            self._print_main_menu()
-            # 
-            option = ''
+            # Faz print do menu principal.
+            print('Fake DB:')
+            print('-- Opção 1 -- Registrar novo usuário')
+            print('-- Opção 2 -- Pesquisar por usuários cadastrados.')
+            print('-- Opção 0 -- Sair')
+            option = input("Escolha uma opção: ")
             if(option == '1'):
                 self._register_user()
             elif(option == '2'):
                 self._select_user_search()
             elif(option == '0'):
                 # termina programa.
+                print("Finalizando.")
                 break
             else:
                 print('Escolha não reconhecida.')
